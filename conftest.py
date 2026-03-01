c7fbbcf2ff65cea1803b75d7081cb24050bae54f
Lina F <buh_19@inbox.ru>
2026-03-01 20:22:28 +0300

    Исправление Faker()

--- conftest.py
+++ conftest.py
@@ -22,7 +22,7 @@ def booking_dates():
 
 @pytest.fixture()
 def generate_random_booking_data(booking_dates):
-    faker = Faker
+    faker = Faker()
     firstname = faker.first_name()
     lastname = faker.last_name()
     totalprice = faker.random_number(digits=3)
